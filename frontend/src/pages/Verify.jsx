3a982f15b6f19006ff0a655778de17e929b0e971
HAvjpro123 <hoanganhbi003@gmail.com>
2024-12-09 13:56:25 +0700

    12/9/2024 - 2

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -5,6 +5,7 @@ import { useSearchParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { Check, ArchiveX  } from 'lucide-react';
 
 const Verify = () => {
 
@@ -25,13 +26,7 @@ const Verify = () => {
 
             if (response.data.success) {
                 setCartItems({})
-                navigate('/orders')
-                setTimeout(() => {
-                    navigate(0)
-                }, 4000);
-            } else {
-                navigate('/cart')
-            }
+            } 
         } catch (error) {
             console.log(error);
             toast.error(error.message)
@@ -39,35 +34,67 @@ const Verify = () => {
         }
     }
 
+    const handlenavigateOrder = () => {
+        navigate('/orders')
+        setTimeout(() => {
+            navigate(0)
+        }, 4000);
+    }
+
+    const handlenavigateCart = () => {
+        navigate('/cart')
+
+        navigate(0)
+    }
     useEffect(() => {
-    }, [token])
+        verifyPayment();
+    }, [success, orderId, token]);
 
     return (
-        <div class="flex items-center justify-center   mt-20 ">
+        <div className="flex items-center justify-center mt-20 ">
+            {
+                success === 'true'
+                    ? <div className="bg-white p-6 rounded-lg border border-gray-200 shadow-lg max-w-sm w-full transform transition-all scale-100 ">
 
-            <div class="bg-white p-6 rounded-lg border border-gray-200 shadow-lg max-w-sm w-full transform transition-all scale-100 ">
+                        <div className="flex items-center justify-center bg-green-100 text-green-600 p-4 rounded-full w-16 h-16 mx-auto shadow-md">
 
-                <div class="flex items-center justify-center bg-green-100 text-green-600 p-4 rounded-full w-16 h-16 mx-auto shadow-md">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"></path>
-                    </svg>
-                </div>
-
-
-                <h2 class="text-center mt-4 text-xl font-semibold text-gray-700">Thanh toán thành công</h2>
-
-                <p class="text-center mt-2 text-gray-500 text-sm">Cảm ơn bạn đã mua sắm! Đơn hàng của bạn đang được xử lý.</p>
+                        <Check size={30} strokeWidth={2}></Check>
+                        </div>
 
 
-                <div class="mt-4 flex justify-center">
-                    <button onClick={verifyPayment} class="px-6 py-2 bg-green-500 hover:bg-green-600 text-white  shadow-lg transition ease-in-out duration-200 transform hover:scale-105">
-                        Xem chi tiết
-                    </button>
-                </div>
-            </div>
+                        <h2 className="text-center mt-4 text-xl font-semibold text-gray-700">Thanh toán thành công</h2>
+
+                        <p className="text-center mt-2 text-gray-500 text-sm">Cảm ơn bạn đã mua sắm! Đơn hàng #{orderId} của bạn đang được xử lý.</p>
+
+
+                        <div className="mt-4 flex justify-center">
+                            <button onClick={handlenavigateOrder}  className="px-6 py-2 bg-green-500 hover:bg-green-600 text-white  shadow-lg transition ease-in-out duration-200 transform hover:scale-105">
+                               Xem chi tiết
+                            </button>
+                        </div>
+                    </div>
+                    : <div className="bg-white p-6 rounded-lg border border-gray-200 shadow-lg max-w-sm w-full transform transition-all scale-100 ">
+
+                        <div className="flex items-center justify-center bg-yellow-100 text-yellow-600 p-4 rounded-full w-16 h-16 mx-auto shadow-md">
+                            <ArchiveX  size={30} strokeWidth={2}></ArchiveX >
+                            
+                        </div>
+
+
+                        <h2 className="text-center mt-4 text-xl font-semibold text-gray-700">Hủy đơn hàng</h2>
+
+                        <p className="text-center mt-2 text-gray-500 text-sm">Đơn hàng #{orderId} của bạn đang được hủy thành công.</p>
+
+
+                        <div className="mt-4 flex justify-center">
+                            <button onClick={handlenavigateCart}   className="px-6 py-2 bg-yellow-500 hover:bg-yellow-600 text-white  shadow-lg transition ease-in-out duration-200 transform hover:scale-105">
+                                Đi tới giỏ hàng
+                            </button>
+                        </div>
+                    </div>
+            }
+
         </div>
-
     )
 }
 
